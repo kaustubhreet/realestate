17d66bc983772db1c3ed07714142531519b22067
kaustubhreet <mishuyashu23@gmail.com>
2023-12-07 17:57:09 +0530

    phone number is added to website and footer fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom';
 import { FaPhone } from "react-icons/fa6";
 
 export default function Footer() {
-    const handleCall = () => {
-        // Replace this with the logic to trigger a call
-        window.location.href = 'tel:+919262685542';
-      };
+    
 
   return (
-    <footer className='bg-slate-200 shadow-md'>
+    <footer className='bg-slate-200' style={{
+      bottom:"0",position:"fixed",left:"0",width:"100%"}}
+    >
     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
       <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
@@ -17,8 +16,8 @@ export default function Footer() {
           <span className='text-slate-700'>HOUSE</span>
         </h1>
       </Link>
-      <Link href="tel:+919262685542">
-      <FaPhone className='text-slate-600'/></Link>
+      
+      <FaPhone className='text-slate-600' onClick={()=>window.open("tel:+91-9262685542")}/>
       </div>
       
      </footer>
